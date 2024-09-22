5f523270628463c250c95ad04ac26b1351ef2e8b
황재웅 <wodnd0131@naver.com>
2024-09-22 11:50:59 +0900

    feat: 로그인, 회원가입 컴포넌트 정리

--- src/page/user/Login.tsx
+++ src/page/user/Login.tsx
@@ -1,90 +1,23 @@
-import { useNavigate } from 'react-router-dom'
-import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import { Card, Title, Input, Button } from '@/component/user/LoginStyles'
+import { LoginContainer } from '@/component/user/LoginContainer'
 
 const Login = () => {
-  const navigate = useNavigate()
-
-  const handleLogin = () => {
-    navigate('/home') // 홈으로 이동
-  }
-
-  const handleSignUp = () => {
-    navigate('/sign-up') // 회원가입으로 이동
-  }
-
   return (
-    <Container>
-      <LoginCard>
+    <LoginContainer>
+      <Card>
         <Title>로그인</Title>
         <Input type="text" placeholder="이메일" />
         <Input type="password" placeholder="비밀번호" />
-        <Button type="button" onClick={handleLogin}>
-          로그인
-        </Button>
-        <LinkButton type="button" onClick={handleSignUp}>
-          회원가입
-        </LinkButton>
-      </LoginCard>
-    </Container>
+        <Link to="/home">
+          <Button>로그인</Button>
+        </Link>
+        <Link to="/sign-up">
+          <Button>회원가입</Button>
+        </Link>
+      </Card>
+    </LoginContainer>
   )
 }
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-color: #f0f0f0;
-`
 
-const LoginCard = styled.div`
-  background: white;
-  padding: 20px;
-  border-radius: 8px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  width: 300px;
-  box-sizing: border-box;
-`
-
-const Title = styled.h2`
-  text-align: center;
-  margin-bottom: 20px;
-`
-
-const Input = styled.input`
-  width: 100%;
-  padding: 10px;
-  margin-bottom: 10px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-`
-
-const Button = styled.button`
-  width: 100%;
-  padding: 10px;
-  background-color: #007bff;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  margin-bottom: 10px; /* 버튼 간의 간격 */
-
-  &:hover {
-    background-color: #0056b3;
-  }
-`
-
-const LinkButton = styled.button`
-  width: 100%;
-  padding: 10px;
-  background-color: transparent;
-  color: #007bff;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-
-  &:hover {
-    text-decoration: underline;
-  }
-`
 export default Login
--- src/page/user/SignUp.tsx
+++ src/page/user/SignUp.tsx
@@ -1,69 +1,21 @@
-import styled from 'styled-components'
-import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { Card, Title, Input, Button } from '@/component/user/LoginStyles'
+import { LoginContainer } from '@/component/user/LoginContainer'
 
 const SignUp = () => {
-  const navigate = useNavigate()
-
-  const handleSignUp = () => {
-    navigate('/home') // 홈으로 이동
-  }
   return (
-    <Container>
-      <LoginCard>
+    <LoginContainer>
+      <Card>
         <Title>회원가입</Title>
         <Input type="text" placeholder="이메일" />
         <Input type="password" placeholder="비밀번호" />
         <Input type="text" placeholder="이름" />
-        <Button type="button" onClick={handleSignUp}>
-          회원 가입
-        </Button>
-      </LoginCard>
-    </Container>
+        <Link to="/">
+          <Button>회원 가입</Button>
+        </Link>
+      </Card>
+    </LoginContainer>
   )
 }
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh; /* 화면 전체 높이 */
-  background-color: #f0f0f0; /* 배경 색상 */
-`
-
-const LoginCard = styled.div`
-  background: white;
-  padding: 20px;
-  border-radius: 8px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  width: 300px; /* 카드 너비 */
-  box-sizing: border-box; /* 패딩 포함한 너비 계산 */
-`
-
-const Title = styled.h2`
-  text-align: center;
-  margin-bottom: 20px;
-`
-
-const Input = styled.input`
-  width: 100%; /* 부모 요소에 맞춰 100% 너비 */
-  padding: 10px;
-  margin-bottom: 10px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box; /* 패딩 포함한 너비 계산 */
-`
-
-const Button = styled.button`
-  width: 100%;
-  padding: 10px;
-  background-color: #007bff;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: #0056b3;
-  }
-`
 export default SignUp
