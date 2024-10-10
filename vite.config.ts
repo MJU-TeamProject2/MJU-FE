3d210b46fe152e6b60c0568bf78672df929b4653
황재웅 <wodnd0131@naver.com>
2024-10-10 15:33:11 +0900

    chore: port 번호를 수정한다.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path-browserify'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, 'src'), // @를 src 폴더로 매핑
     },
   },
   server: {
-    port: 5174,
+    port: 5173,
   },
 })
